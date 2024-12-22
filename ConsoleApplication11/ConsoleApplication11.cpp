a22d7d8d59241ed148b628a894854fd158138fd4
Lenovo <Lenovo@DESKTOP-1RER6FA>
2024-12-22 21:33:17 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,48 @@
+﻿//#include <iostream>
+//using namespace std;
+//int main() {
+//    const int size = 5;
+//    int arr1[size] = { 1, 2, 3, 4, 5 };
+//    int arr2[size];
+//    int* p1 = arr1;
+//    int* p2 = arr2;
+//    for (int i = 0; i < size; ++i) {
+//        *(p2++) = *(p1++);
+//    }
+//    for (int i = 0; i < size; ++i) {
+//        cout << arr2[i] << " ";
+//    }
+//    return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main() {
+//    const int size = 5;
+//    int arr[size] = { 1, 2, 3, 4, 5 };
+//    int* p = arr + size - 1;
+//    while (p >= arr) {
+//        cout << *(p--) << " ";
+//    }
+//    return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    const int size = 5;
+//    int arr1[size] = { 1, 2, 3, 4, 5 };
+//    int arr2[size];
+//    int* p1 = arr1 + size - 1;
+//    int* p2 = arr2; 
+//    for (int i = 0; i < size; ++i) {
+//        *(p2++) = *(p1--);
+//    }
+//    for (int i = 0; i < size; ++i) {
+//        cout << arr2[i] << " ";
+//    }
+//    return 0;
+//}
